8c9136f325324a6cdb9f54d7ca1f8756cc68042a
Saad-Ahsan626 <saadahsan039@gmail.com>
2026-04-21 17:56:50 +0500

    HomeScreen With Emergencies

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,7 +1,10 @@
 import 'package:first_aid_assisstant/providers/emergency_provider.dart';
+import 'package:first_aid_assisstant/widgets/emergency_card.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class Homescreen extends ConsumerStatefulWidget {
   const Homescreen({super.key});
@@ -13,33 +16,111 @@ class Homescreen extends ConsumerStatefulWidget {
 class _HomescreenState extends ConsumerState<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    final EmergencyState = ref.watch(EmergencyProvider);
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text("First Aid Assistant"))),
-      body: EmergencyState.when(
-        data: (emergencies) {
-          if (emergencies.isEmpty) {
-            return const Center(child: Text('No emergencies found.'));
-          }
-          return ListView.builder(
-            itemCount: emergencies.length,
-            itemBuilder: (context, index) {
-              final emergency = emergencies[index];
-              return ListTile(
-                leading: const Icon(Icons.medical_services, color: Colors.red),
-                title: Text(
-                  emergency.titleEn,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
-                ),
-                subtitle: Text(emergency.titleUr),
-              );
-            },
-          );
-        },
-        error: (error, Stack) =>
-            Center(child: Text('Oops, something went wrong: $error')),
-        loading: () => const Center(child: CircularProgressIndicator()),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Color(0xFF129490)),
+              child: Text(
+                'First Aid Assistant',
+                style: TextStyle(color: Colors.white, fontSize: 24),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(CupertinoIcons.home),
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(CupertinoIcons.info),
+              title: const Text('About'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: const Icon(CupertinoIcons.brightness),
+              title: const Text('Change Theme'),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
       ),
+      appBar: AppBar(
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                icon: const Icon(CupertinoIcons.search),
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+        backgroundColor: const Color(0xFF129490),
+        title: Center(
+          child: Text(
+            "First Aid Assistant",
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Roboto',
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.white70],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Consumer(
+          builder: (context, ref, child) {
+            final emergencyList = ref.watch(EmergencyProvider);
+
+            return Expanded(
+              child: emergencyList.when(
+                loading: () => const Center(child: CircularProgressIndicator()),
+                error: (error, stack) => Center(child: Text('Error: $error')),
+                data: (emergencies) {
+                  if (emergencies.isEmpty) {
+                    return const Center(child: Text('No emergencies found'));
+                  }
+                  return SingleChildScrollView(
+                    padding: const EdgeInsets.all(12.0),
+                    child: StaggeredGrid.count(
+                      crossAxisCount: 2,
+                      mainAxisSpacing: 12,
+                      crossAxisSpacing: 12,
+                      children: List.generate(emergencies.length, (index) {
+                        final emergency = emergencies[index];
+
+                        final int columnSpan = (index % 3 == 0) ? 2 : 1;
+
+                        return StaggeredGridTile.fit(
+                          crossAxisCellCount: columnSpan,
+                          child: Emergency_card(emergency, columnSpan == 2),
+                        );
+                      }),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        ),
+      ),
+      
     );
   }
 }
